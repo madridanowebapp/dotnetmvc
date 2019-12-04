0b816140530f538c35b256689a86f7734e7cdd2a
madridanowebapp <58509105+madridanowebapp@users.noreply.github.com>
2019-12-04 03:29:52 -0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharpDevelopMVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+		ViewBag.Message = "Hello message from Index!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+        	ViewBag.Message = "Hello message from about!";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+			ViewBag.Message = "Hello message from Contact!";
+            return View();
+        }
+        public ActionResult Input()
+        {
+
+			ViewBag.Message = "Hello message from Input!";
+            return View();
+        }
+        public ActionResult TransferSave(  
+            string firstname,
+			string lastname,
+			string transactionType,
+			string amount,
+			string message,
+			string radioBtnTransactOption,
+			string dateTransfer)
+        {
+	
+			ViewBag.Firstname = firstname;
+			ViewBag.Lastname = lastname;
+			ViewBag.TransactionType = transactionType;
+			ViewBag.Amount = amount;
+			ViewBag.Message = message;
+			ViewBag.Transaction = radioBtnTransactOption;	
+			ViewBag.DateOfTransfer = dateTransfer;	
+            return View();
+        }
+         
+        public ActionResult Transfer()
+        {
+        	return View();
+		}
+    }
+}
